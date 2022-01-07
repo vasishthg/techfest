283b5af39ceb79ce8b70c843b1fdfbea8e582593
Vasishth Gupta <vasishthemail@gmail.com>
2022-01-07 18:49:06 +0530

    e

--- app.py
+++ app.py
@@ -2,101 +2,80 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import time
 from werkzeug.utils import redirect
+import logging
+import socket
 
 app = Flask(__name__)
+hostname = socket.gethostname()
+IPaddr = socket.gethostbyname(hostname)
+logging.basicConfig(filename='log.log', encoding='utf-8')
+details = ('Visitor: ', hostname, 'IP Address: ', IPaddr)
+logging.error(details)
 
-# Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = 'your secret key'
 
-# Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'toor'
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_DB'] = 'metset'
 
-# Intialize MySQL
 mysql = MySQL(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
-    msg = ''
-        # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
+    msg=''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
         password = request.form['password']
-                # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-                # If account exists in accounts table in out database
+        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password =%s', (email,password))
+        account=cursor.fetchone()
+        
         if account:
-            # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
+            session['email'] = account['email']
+            msg = 'Logged in succesfully'
+            time.sleep(1)
             return redirect('/')
         else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    return render_template('login.html', msg='')
+            msg='Incorrect email/password'
+    return render_template('login.html', msg=msg)
 
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET','POST'])
 def register():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-                # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
-    elif request.method == 'POST':
-        # Form is empty... (no POST data)
-        msg = 'Please fill out the form!'
-        
+    if request.method == 'POST' and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form and 'password' in request.form:
+       firstname = request.form['firstname']
+       lastname = request.form['lastname']
+       email = request.form['email']
+       password = request.form['password']
+       cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+       cursor.execute('SELECT * FROM accounts WHERE email = %s', (email,))
+       account = cursor.fetchone()
+
+       if account:
+        msg = 'Email already exists. Please Login.'
+       elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        msg = 'Invalid email address'
+       elif not firstname or not email or not lastname or not password:
+           msg='Please fill in all the details.'
+       else:
+           cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s)', (firstname, lastname, email, password))
+           mysql.connection.commit()
+           msg = 'Registration successful.'
     
+    elif request.method == 'POST':
+        msg='Registration Incomplete!'
 
-    # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
-
-
-
-@app.route('/logout')
-def logout():
-    # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
-
 if __name__ == "__main__":
     app.run(debug=True)
